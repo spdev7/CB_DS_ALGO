187060324e18dffc7fbe4057d05ff08921e5f3df
Saurav Prakash <sauravprakash7@outlook.com>
2018-06-11 16:01:47 +0530

    Odd even recursion

---
+++ Recursion/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+    if(n <0)return ;
+    else{
+        cout<<n<<endl;
+        print1(n-1);
+    }
+
+}
+
+int main (){
+    int n;
+    cin>>n;
+    print1(n);
+}
+
+//while solving cb recurssion assignment Algo++ Summer 2018 Practice Contest
+//ODD EVEN USING RECURSION
+//in this we will get from n to 0 DOUBT THIS WAS NOT WORKING WITH WHILE STATEMENT
