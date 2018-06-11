05d5ff431dc4b6b788c3a2e8a280abfc9d43fe4a
Saurav Prakash <sauravprakash7@outlook.com>
2018-06-11 17:15:38 +0530

    PHEW

---
+++ Recursion/2.cpp
@@ -0,0 +1,35 @@
+//EVEN ODD RECURSION
+//FIRST PROPER SUBMISSION
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+    if(n <0)return ;
+    else{
+        if(n%2!=0)
+        cout<<n<<endl;
+        print1(n-1);
+    }
+
+}
+
+void print2(int n){
+    if(n>1)
+    {
+         if(n % 2 == 0)
+         {
+         print2(n-1);
+         cout<<n<<endl;}
+         else
+             print2(n-1);
+
+    }
+
+}
+
+int main (){
+    int n;
+    cin>>n;
+    print1(n);
+    print2(n);
+}
